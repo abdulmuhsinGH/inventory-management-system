d7d267fcf20d4a401cf3355a36f6f849ee750e7d
Abdul Muhsin Iddris <abdul.iddris13@gmail.com>
2017-07-14 09:05:43 +0000

    code rafactoring. calculate additional expense and income

--- src/api/controllers/financial-report/income-statement.js
+++ src/api/controllers/financial-report/income-statement.js
@@ -4,17 +4,13 @@ var moment = require('moment');
 
 var startDate = '2017-03-01';
 var endDate = '2017-06-30';
-//var 
-
+var incomeStatementJSON = {};
 //var IncomeStatementClass = class IncomeStatement
 
-var getTotalSalesAmount = (startDate, endDate) => {
-    var result = {};
-    var totalSalesAmountQuery = `select sum(transaction_logs.total_sales_amount) as total_sales_amount 
-                                 from transaction_logs, sales
-                                 where transaction_logs.sales_id = sales.id and transaction_logs.created_at between '${startDate}' and '${endDate}'`;
+var getQueryDataPromise = (query) => {
+
     return new Promise((resolve, reject) => {
-        db.all(totalSalesAmountQuery, function (err, rows) {
+        db.all(query, (err, rows) => {
             if (err) {
                 reject("Unable to fetch data");
             }
@@ -25,49 +21,59 @@ var getTotalSalesAmount = (startDate, endDate) => {
         });
 
     });
+
+}
+var calculateTotalSalesAmount = (startDate, endDate) => {
+    var result = {};
+    var totalSalesAmountQuery = `select sum(transaction_logs.total_sales_amount) as total_sales_amount 
+                                 from transaction_logs, sales
+                                 where transaction_logs.sales_id = sales.id and transaction_logs.created_at between '${startDate}' and '${endDate}'`;   
    
-    
+    getQueryDataPromise(totalSalesAmountQuery).then((totalSales) => {
+        console.log(totalSales);
+
+        incomeStatementJSON["sales"] = totalSales;
+    })
 
     	
 };
 
-
-var getTotalPurchasesAmount = (startDate, endDate)=>{
+var calculateTotalPurchasesAmount = (startDate, endDate)=>{
     var totalPurchasesAmountQuery = `select sum(inventory_details.cost_price * inventory_details.quantity) as total_purchase_amount 
                                      from inventory_details
                                      where inventory_details.created_at between '${startDate}' and '${endDate}'`;
 
     var result = {};
-    return new Promise((resolve, reject) => {
-        db.all(totalPurchasesAmountQuery, function (err, rows) {
-            if (err) {
-                reject("Unable to fetch data");
-            }
-            else {
-                resolve(rows)
-            }
-
-        });
-
+    getQueryDataPromise(totalPurchasesAmountQuery).then((totalPurchases) => {
+        console.log(totalPurchases);
+        incomeStatementJSON["cost_of_goods_sold"] = totalPurchases;
     });
 
    	
 
 };
 
-var calculateGrossProfit = function (startDate, endDate) {
-    var grossProfit = 0;
+var calculateTotalAmountofAdditionalRecord = (startDate, endDate, record_type_id) => {
+    var totalAdditionalRecordQuery = `select sum(amount) as total_amount, additional_record_type.name from additional_record_logs 
+                                      join additional_record_logs on additional_record_logs.record_type_id_fk = additional_record_type.id
+                                      where additional_record_logs.record_type_id_fk = ${record_type_id} and additional_record_logs.created_at between '${startDate}' and '${endDate}' group by additional_record_type.name`;
 
-
-    getTotalSalesAmount(startDate, endDate).then((totalSalesAmount) => {
-        grossProfit += Number(totalSalesAmount[0].total_sales_amount);
-        return getTotalPurchasesAmount(startDate, endDate)
-    }).then((totalPurchasesAmount) => {
-        grossProfit = grossProfit - Number(totalPurchasesAmount[0].total_purchase_amount);
-
-        })
-    return grossProfit; 
+    getQueryDataPromise(totalAdditionalRecordQuery).then((totalAdditionalRecord) => {
+        console.log(totalAdditionalRecordQuery);
+        var recordTypeName = totalAdditionalRecord[0].name;
+        incomeStatementJSON[recordTypeName] = totalAdditionalRecord[0].total_amount
+    });
 }
-  
-var currentProfit = calculateGrossProfit(startDate, endDate);
-console.log(currentProfit);
+
+var calculateGrossProfit = () => {
+    var grossProfit = 0;
+    grossProit = incomeStatementJSON.sales - incomeStatementJSON.cost_of_goods_sold;
+
+    incomeStatementJSON["gross_profit"] = grossProfit;
+
+};
+
+calculateTotalSalesAmount(startDate, endDate);
+calculateTotalPurchasesAmount(startDate, endDate);
+
+calculateGrossProfit()
